add42d1a808bea33a92c7f0a5701ad818d433515
sy-md <mdorsetti733@gmail.com>
2023-02-28 13:43:44 -0700

    all done

--- hotel.py
+++ hotel.py
@@ -12,34 +12,49 @@ class hotel:
         self.operation = operation
 
     def booking(self):
+        king = self.king
+        queen = self.queen
         genders = ["sir","ma'ma"]
         room = ["King", "Queen"]
         rm = random.choice(room)
         print("Welcome {} which room would you like. one {} size room".format(random.choice(genders), rm))
-
         if rm == "King":
-            res = self.king - 1
-            print("{} king size room left".format(self.king))
-            return hotel.pay(self, res, booked="King")
+            return hotel.avaiable(self, rm, king)
         else:
-            res = self.queen - 1
-            print("{} queen size room left".format(self.queen))
-            return hotel.pay(self, res, booked="Queen")
+            return hotel.avaiable(self, rm, queen)
 
     def pay(self, rm, booked=None):
+        if self.king <= 1:
+            print("sorry no more avaiable")
+            profit = 0
+            return (profit)
+        if self.queen == 1:
+            print("sorry no more avaiable")
+            profit = 0
+            return (profit)
+
 
         if booked == "King":
-            price = (170 * (self.king - rm))
-            profit = price
+            amount_of_room = (self.king - (self.king - 1))
+            profit = (170 * amount_of_room)
+            return profit
         if booked == "Queen":
-            amount_of_room = (self.queen - rm)
-            price = (150 * amount_of_room)
-            profit = price
+            amount_of_room = (self.queen - (self.queen - 1))
+            profit = (150 * amount_of_room)
+            return profit
 
-        return (profit)
 
-    def avaiable(self):
-        return self.amount
+    def avaiable(self, typ, opt=None):
+        res = opt - 1
+        if res == 0:
+            return hotel.pay(self, res, booked=None)
+            
+        print("{} {} size rooms left".format(res, typ))
+        if typ == "King":
+            self.king -= 1
+        else:
+            self.queen -= 1
+        return hotel.pay(self, res, booked=typ)
 
     def paycheck(self):
         monthly = 8500
--- main.py
+++ main.py
@@ -61,74 +61,41 @@ def main():  # create data and tables
         tables = [create_hotel, create_emp, create_bank]
         updates = [update_bank]
 
-        my_hotel = hotel(title="jumper", king=10, queen=10, amount=(20), title=hotel_title, operation=tables[0])
+        my_hotel = hotel(title="jumper", king=10, queen=10, amount=(20), operation=tables[0])
         b = bank(operation=tables[2])
-        """
-        setting the database for the first hotel and bank
-            * this can be a function 
-         
-        hotel_data = my_hotel.get_report() #getting the first report
-        avaiable = hotel.avaiable(my_hotel) #checking for rooms
-        bank_data = b.get_statement() # getting the statment from the bank
-        
-        data = tuple((hotel_data, bank_data)) # i think i should add avaiable
-        """
-        
-        """def setupdatabe(conn, data, tables )""" #send data to get processed
-        insert_table(conn, data, tables) # get rid of this line of code and relpace with setupdatabase()
-        
-        # exit()  #uncomment to just test database connection 
-        while True: # life
-            """
-            1.) have guess check in the hotel
-            2.) do we have any room avaiable
-                
-            
-            def func():
-                x = "king"
-                if x == "queen":
-                    print("here u go")
-                else:
-                    print("no queen sorry")
-                    return test(x)
-            
-            def test(x):
-                print(x)
-                return x
 
-            while True:
-                for x in range(1):
-                    profit = func()
-                    print("back in while loop")
-                    print(profit)
-                break
+        hotel_data = my_hotel.get_report()
+        bank_data = b.get_statement()
+        data = tuple((hotel_data, bank_data))
 
-            > no queen sorry
-            > back in while loop
-            
-            
-    
-                
-    
-            """
-            for _ in range(5): # 20 days for :eon month 
-                monthly_profit = 0
-                time.sleep(0.8) 
+        insert_table(conn, data, tables)
+
+        # exit()  #uncomment to just test database connection
+
+        """
+
+        monthly profit keeps being 0
+
+        re failing
+
+        """
+
+        while True:  # life
+            for _ in range(30):  # sim a month
+                profit = 0
+                time.sleep(0.8)
                 re = hotel.booking(my_hotel)
-                monthly_profit += re
-                ava = hotel.avaiable(my_hotel)
-                print(ava)
-                if ava <= 0:
-                    break
-                time.sleep(0.5)
-                print("profit of the month - ", re)
+                if re != 0 and re is not None:
+                    profit += re
+                    print("profit - $", re)
 
-                bank_data = (bank_data + monthly_profit)
+                bank_data = (bank_data + profit)
                 update_table(conn, bank_data, updates)
                 mnthly = hotel.paycheck(my_hotel)
                 print("monthly pay $", mnthly)
                 bank.withdraw_money(b, mnthly)
-            
+            break
+
     else:
         print("Error! cannot create the database connection.")
 
